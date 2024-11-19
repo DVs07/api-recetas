b88056a7b550bfc01cbf42862eeb6c5e6227628a
DVs07 <aldopablo7@hotmail.com>
2024-11-19 20:46:22 -0300

    Se agrego la funcionalidad al boton del modal Guardar Favorito,muestra un toast(propio de bootstrap) con un mensaje segun sea el caso(Guardar/Eliminar). En la pagina donde se muestran las recetas Favoritas, obtenemos los datos desde el localStorage y desde la misma API, se reutilizaron funciones para inyectarlas en el HTML, eliminar una receta desde Favoritos se hace en tiempo real (tambien se reutilizaron funciones para esta accion).

--- js/app.js
+++ js/app.js
@@ -4,9 +4,15 @@ function iniciarApp() {
     const contenedorRecetas = document.querySelector('#resultado');
     const modal = new bootstrap.Modal('#modal',{});
 
-    selectCategorias.addEventListener('change', seleccionarCategoria);
-
-    obtenerCategorias();
+    if(selectCategorias){
+        selectCategorias.addEventListener('change', seleccionarCategoria);
+        obtenerCategorias();
+    }
+    
+    const favoritosDiv = document.querySelector('.favoritos');
+    if(favoritosDiv){      
+        obtenerFavoritos();
+    }
 
     function obtenerCategorias(){
         const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
@@ -77,7 +83,7 @@ function iniciarApp() {
             const imgReceta = document.createElement('img');
             imgReceta.classList.add('card-img-top');
             imgReceta.alt = `Imagen de la receta ${strMeal}`;
-            imgReceta.src = strMealThumb;
+            imgReceta.src = strMealThumb ?? receta.imagen;
             
             // console.log(imgReceta);
 
@@ -88,7 +94,7 @@ function iniciarApp() {
             // Crear el titulo
             const heading = document.createElement('h3');
             heading.classList.add('card-title');
-            heading.textContent = strMeal;
+            heading.textContent = strMeal ?? receta.titulo;
             // console.log(heading.outerHTML); // Muestra el html de la card
             // console.log(heading.outerText); // Muestra el texto de la card
             
@@ -99,7 +105,7 @@ function iniciarApp() {
             // boton.dataset.bsTarget = '#modal';
             // boton.dataset.bsToggle = 'modal';
             boton.onclick = function (){
-                verReceta(idMeal);
+                verReceta(idMeal ?? receta.id);
             };
 
             // Renderizar la card
@@ -168,7 +174,23 @@ function iniciarApp() {
         // Botonones cerrar y favorito
         const btnFavorito = document.createElement('button');
         btnFavorito.classList.add('btn', 'btn-danger','col');
-        btnFavorito.textContent = 'Guardar Favorito';
+        btnFavorito.textContent = existeStorage(idMeal) ? 'Eliminar Favorito' : 'Guardar Favorito';
+
+        // LocalStorage
+        btnFavorito.onclick = function(){
+            // console.log(existeStorage(idMeal));
+
+            if(existeStorage(idMeal)){
+                eliminarFavorito(idMeal);
+                btnFavorito.textContent = 'Guardar Favorito';
+                mostrarToast('Eliminado Correctamente');
+                return;
+            }
+            
+            agregarFavorito({id: idMeal, titulo: strMeal, imagen: strMealThumb});
+            btnFavorito.textContent = 'Eliminar Favorito';
+            mostrarToast('Agregado a Favoritos');
+        }
 
         const btnCerrar = document.createElement('button');
         btnCerrar.classList.add('btn', 'btn-secondary','col');
@@ -177,11 +199,62 @@ function iniciarApp() {
             modal.hide();
         };
 
+        limpiarHTML(modalFooter);
         modalFooter.appendChild(btnFavorito);
         modalFooter.appendChild(btnCerrar);
         
         // Muestro el Modal
         modal.show();
+    }
+
+    function agregarFavorito(receta){
+        // console.log('Agregando a favoritos');
+        // console.log(receta);
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        localStorage.setItem('favoritos', JSON.stringify([...favoritos, receta] ));        
+
+        if(favoritosDiv){      
+            obtenerFavoritos();
+        }
+    }
+
+    function existeStorage(idReceta){
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        return favoritos.some(favorito => favorito.id === idReceta);
+    }
+
+    function eliminarFavorito(idReceta){
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        const nuevosFavoritos = favoritos.filter(favorito => favorito.id !== idReceta);
+        localStorage.setItem('favoritos', JSON.stringify(nuevosFavoritos));
+
+        if(favoritosDiv){      
+            obtenerFavoritos();
+        }
+    }
+
+    function mostrarToast(mensaje){
+        const toastDiv = document.querySelector('#toast');
+        const toastBody = document.querySelector('.toast-body');
+        const toast = new bootstrap.Toast(toastDiv);
+        toastBody.textContent = mensaje;
+        toast.show();
+    }
+
+    function obtenerFavoritos(){
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        // console.log(favoritos);
+        let anterior = favoritos.length;
+        if(favoritos.length>0){
+            mostrarRecetas(favoritos);
+            return;
+        }
+
+        const sinFavoritos = document.createElement('p');
+        sinFavoritos.textContent = 'No hay favoritos';
+        sinFavoritos.classList.add('fs-4', 'fw-bold', 'mt-5');
+        contenedorRecetas.appendChild(sinFavoritos);
+        
     }
 
     function limpiarHTML(selector){
